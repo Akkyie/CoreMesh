8a4b1dc4364653e0313aa0433d56baf13b89cd93
Akio Yasui <akkyie01@gmail.com>
2015-10-22 19:31:09 +0900

    Initial Commit

---
+++ CoreMesh/CoreMesh.h
@@ -0,0 +1,19 @@
+//
+//  CoreMesh.h
+//  CoreMesh
+//
+//  Created by Akio Yasui on 10/22/15.
+//  Copyright © 2015 Akio Yasui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreMesh.
+FOUNDATION_EXPORT double CoreMeshVersionNumber;
+
+//! Project version string for CoreMesh.
+FOUNDATION_EXPORT const unsigned char CoreMeshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreMesh/PublicHeader.h>
+
+
